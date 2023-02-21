9c6b9f4774906d2ac7a81f2500e315e41a721543
Vilma <vilmail@protonmail.com>
2023-02-21 22:17:27 +0200

    fixed issues, left formatting

--- main.js
+++ main.js
@@ -1,11 +1,8 @@
 "use strict";
 const game = {
-  playerPc: new Player(),
-  playerHuman: null,
-  roundSeconds: 10,
-  roundsCount: 10,
-  timerEl: document.querySelector("#timer-counter"),
-  humanStatsNameEl: document.querySelector(".human-stats .name"),
+  pcPlayer: new Player(),
+  humanPlayer: null,
+  roundsAmount: 5,
   square: {
     element: document.getElementById("square"),
     container: document.getElementById("stage"),
@@ -18,16 +15,16 @@ const game = {
       this.element.style.backgroundColor = `rgb(${red}, ${green}, ${blue})`;
     },
     changePos: function () {
-      console.log(this);
       let newX = rand(0, this.container.offsetWidth - 50);
       let newY = rand(0, this.container.offsetHeight - 50);
       this.element.style.top = `${newY}px`;
       this.element.style.left = `${newX}px`;
     },
     update: function () {
+      this.element.classList.remove("clicked");
+      this.clicked = false;
       this.changeColor();
       this.changePos();
-      this.element.classList.remove("clicked");
     },
     listenForClicks() {
       this.element.addEventListener("mousedown", () => {
@@ -37,59 +34,82 @@ const game = {
     },
   },
   round: {
+    id: 0,
+    seconds: 5,
     intervalId: null,
-    time: null,
+    timeLeft: this.seconds,
+    timerEl: document.querySelector("#timer"),
+    timeDecrease() {
+      this.timeLeft--;
+      this.updateTimer();
+    },
+    end() {
+      this.timerEl.classList.add("hidden");
+      clearInterval(this.intervalId);
+    },
+    start() {
+      this.timeLeft = this.seconds;
+      this.timerEl.classList.remove("hidden");
+      this.updateTimer();
+      this.id++;
+    },
+    updateTimer() {
+      this.timerEl.textContent = this.timeLeft.toString().length > 1 ? this.timeLeft + "s" : "0" + this.timeLeft.toString() + "s";
+    },
   },
   play: function () {
-    this.round.time = this.roundSeconds;
+    console.log("playing");
+    this.round.start();
+    this.square.update();
+    this.square.element.classList.remove("hidden");
     this.round.intervalId = setInterval(() => {
-      if (this.round.time <= 0) {
-        clearInterval(this.round.intervalId);
-        this.playerHuman.savePoints();
-        this.playerPc.savePoints();
-        this.views.showRoundStats(this.playerPc, this.playerHuman);
+      this.updateScore();
+      if (this.round.timeLeft === 1) {
+        //if on last second
+        this.humanPlayer.savePoints();
+        this.pcPlayer.savePoints();
+        this.round.end();
         this.square.element.classList.add("hidden");
-        this.round.time = this.roundSeconds; //reset
-        if (this.playerPc.history.length < this.roundsCount) {
-          console.log(this.playerPc.history, "history");
+        this.views.showRoundStats(this.pcPlayer, this.humanPlayer);
+        if (this.round.id < this.roundsAmount) {
+          //if more rounds left, start another round
           setTimeout(() => {
             game.views.hideRoundStats();
             this.square.element.classList.remove("hidden");
             game.play(); //set interval again
-          }, 3000);
+          }, 5000);
+          // return;
         } else {
+          //end of game
           let winner = "It is a draw!";
-          if (this.playerHuman.totalPoints > this.playerPc.totalPoints) {
-            winner = this.playerHuman.name;
-          } else {
-            winner = this.playerPc.name;
+          if (this.humanPlayer.totalPoints > this.pcPlayer.totalPoints) {
+            winner = this.humanPlayer.name;
+          } else if (this.humanPlayer.totalPoints < this.pcPlayer.totalPoints) {
+            winner = this.pcPlayer.name;
           }
           this.views.winnerNameEl.textContent = winner;
-          this.views.switchView("end");
+          this.views.showRoundStats(this.pcPlayer, this.humanPlayer, true); //true for end of game
+          // return;
         }
       }
-      this.updateScore();
       this.square.update();
-      this.timerEl.textContent = this.round.time;
-
-      this.round.time--;
+      this.round.timeDecrease();
     }, 1000);
   },
   updateScore() {
     if (this.square.clicked) {
-      this.playerHuman.currentRoundPoints++;
-      this.square.clicked = false;
+      this.humanPlayer.currentRoundPoints++;
     } else {
-      this.playerPc.currentRoundPoints++;
+      this.pcPlayer.currentRoundPoints++;
     }
   },
   views: {
     start: document.querySelector(".initial-view"),
     inGame: document.querySelector(".game-view"),
-    end: document.querySelector(".end-view"),
     current: "start",
     /////
     roundStats: document.querySelector(".round-stats"),
+    endStats: document.querySelector(".game-end-stats"),
     humanRoundStatsEl: document.querySelector(".human-stats .rounds"),
     humanTotalPointsEl: document.querySelector(".human-stats .total"),
     pcTotalPointsEl: document.querySelector(".pc-stats .total"),
@@ -102,12 +122,19 @@ const game = {
         this.current = viewStr;
       }
     },
-    showRoundStats: function (playerPc, playerHuman) {
+    showRoundStats: function (pcPlayer, humanPlayer, gameOver = false) {
       this.roundStats.classList.remove("hidden");
-      this.humanTotalPointsEl.textContent = playerHuman.totalPoints;
-      this.pcTotalPointsEl.textContent = playerPc.totalPoints;
-      this.humanRoundStatsEl.innerHTML += `<li>${playerHuman.history[playerHuman.history.length - 1]}</li>`;
-      this.pcRoundStatsEl.innerHTML += `<li>${playerPc.history[playerPc.history.length - 1]}</li>`;
+      if (gameOver) {
+        this.endStats.classList.remove("hidden");
+      } else {
+        this.humanTotalPointsEl.textContent = humanPlayer.totalPoints;
+        this.pcTotalPointsEl.textContent = pcPlayer.totalPoints;
+        let humanRoundPoints = humanPlayer.history[humanPlayer.history.length - 1];
+        let pcRoundPoints = pcPlayer.history[pcPlayer.history.length - 1];
+        let humanWinner = humanRoundPoints > pcRoundPoints ? true : false;
+        this.humanRoundStatsEl.innerHTML += `<li class=${humanWinner ? "green" : ""}>${humanRoundPoints}</li>`;
+        this.pcRoundStatsEl.innerHTML += `<li class=${humanWinner ? "" : "green"}>${pcRoundPoints}</li>`;
+      }
     },
     hideRoundStats: function () {
       this.roundStats.classList.add("hidden");
@@ -117,19 +144,24 @@ const game = {
     if (e.type === "submit") {
       //if first time
       e.preventDefault();
-      let playerName = getFormData(e.target).name;
-      this.playerHuman = new Player(playerName);
-      this.humanStatsNameEl.textContent = playerName;
+      let formData = getFormData(e.target);
+      let playerName = formData.name;
+      if (playerName === "PC") {
+        return alert("Name is taken");
+      }
+      this.humanPlayer = new Player(playerName);
+      this.round.seconds = formData.seconds;
+      this.roundsAmount = formData.rounds;
       this.square.listenForClicks();
+      this.views.switchView("inGame");
     }
-    console.log(this);
-    this.views.switchView("inGame");
     this.views.hideRoundStats();
-    this.square.element.classList.remove("hidden");
+    this.views.endStats.classList.add("hidden");
     this.views.humanRoundStatsEl.innerHTML = "";
     this.views.pcRoundStatsEl.innerHTML = "";
-    this.playerHuman.reset();
-    this.playerPc.reset();
+    this.humanPlayer.reset();
+    this.pcPlayer.reset();
+    this.round.id = 0;
     this.play();
   },
 };
@@ -145,14 +177,15 @@ function getFormData(form) {
   const formData = new FormData(form);
   let data = {};
   for (const [key, value] of formData) {
-    console.log(key, value);
     data[key] = value;
   }
   return data;
 }
 function Player(name = "PC") {
   this.name = name;
-  // this.roundsWon = 0;
+  if (name !== "PC") {
+    document.querySelector(".human-stats .name").textContent = name;
+  }
   this.totalPoints = 0;
   /////FIX: nox need to save all rounds to history as using + innerHtml anyway
   this.history = [];
@@ -164,7 +197,6 @@ function Player(name = "PC") {
     this.currentRoundPoints = 0; //clear for next round
   };
   this.reset = function () {
-    // this.roundsWon = 0;
     this.totalPoints = 0;
     this.history.length = 0;
     this.currentRoundPoints = 0;
